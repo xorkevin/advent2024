960c3ab3a78883e467a7831a83db33b5c348d160
Kevin Wang <kevin@xorkevin.com>
2024-11-30 20:09:20 -0800

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/xorkevin/advent2024
+
+go 1.23.3
---
+++ tpl/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+const (
+	puzzleInput = "input.txt"
+)
+
+func main() {
+	file, err := os.Open(puzzleInput)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		num, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Println(num)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatalln(err)
+	}
+}
